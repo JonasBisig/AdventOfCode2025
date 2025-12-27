39421194653b687e72b6038012a451f16c32b83c
Jonas Bisig <jonas.bisig@gmx.ch>
2025-12-27 17:52:07 +0100

    First Day Part 2 Finished

--- AdventToCode/Program.cs
+++ AdventToCode/Program.cs
@@ -5,12 +5,17 @@ namespace AdventToCode
     {
         static void Main(string[] args)
         {
+            //example string
+            string example = "L68,L30,R48,L5,R60,L55,L1,L99,R14,L82";
+
             //Read Input File
-            IEnumerable<string> inputLines = File.ReadLines("C:\\Users\\jonas\\source\\repos\\JonasBisig\\AdventToCode\\AdventToCode\\bin\\input.txt");
+            //to use the example string enable this line and comment the line below
+            //IEnumerable<string> inputLines = example.Split(",");
+            IEnumerable<string> inputLines = File.ReadLines(Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName, "input.txt"));
 
             //Dial starts at 50
             int dial = 50;
-            int zeroCount = 0;
+            int password = 0;
             foreach (string line in inputLines)
             {
                 //Extract Direction -> R = +, L = -
@@ -22,25 +27,39 @@ namespace AdventToCode
                 //Add or Subtract the clicks from the dial
                 if (direction == "R")
                 {
-                    dial += clicks;
+                    //Make a for loop for clicks many times and increment dial each time
+                    for (int i = 0; i < clicks; i++)
+                    {
+                        dial++;
+                        //reset dial if it overflows and increment password
+                        if (dial == 100)
+                        {
+                            dial = 0;
+                            password++;
+                        }
+                    }                    
                 }
                 else
                 {
-                    dial -= clicks;
-                }
-
-                //limit dial value from 0 - 99
-                dial = (dial % 100 + 100) % 100;
-
-                //Increment the zeroCount Value
-                if (dial == 0)
-                {
-                    zeroCount++;
+                    //Make a for loop for clicks many times and decrement dial each time
+                    for (int i = 0; i < clicks; i++)
+                    {
+                        dial--;
+                        //if dial is at 0 increment password and if its overflowing reset dial to 99
+                        if (dial == 00)
+                        {
+                            password++;
+                        }
+                        else if (dial == -1)
+                        {
+                            dial = 99;
+                        }
+                    }
                 }
             }
 
             //Display the final Password
-            Console.WriteLine(zeroCount);
+            Console.WriteLine(password);
         }
     }
 }
