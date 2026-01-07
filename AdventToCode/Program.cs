d5b3450b04e353bb05353b123f0c8c2160c789f1
JonasBisig <jonas.bisig@gmx.ch>
2026-01-07 17:57:23 +0100

    Eighth Day Part 1 not finished

--- AdventToCode/Program.cs
+++ AdventToCode/Program.cs
@@ -10,11 +10,32 @@ namespace AdventToCode
         static void Main(string[] args)
         {
             //example string
-            string example = "";
+            string example = """
+                162,817,812
+                57,618,57
+                906,360,560
+                592,479,940
+                352,342,300
+                466,668,158
+                542,29,236
+                431,825,988
+                739,650,466
+                52,470,668
+                216,146,977
+                819,987,18
+                117,168,530
+                805,96,715
+                346,949,466
+                970,615,88
+                941,993,340
+                862,61,35
+                984,92,344
+                425,690,689
+                """;
 
             //to use the example string enable this line and comment the line below
-            //IEnumerable<string> array = example.Split(",");
-            IEnumerable<string> array = File.ReadLines(Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName, "input.txt"));
+            IEnumerable<string> array = example.Split("\r\n");
+            //IEnumerable<string> array = File.ReadLines(Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName, "input.txt"));
         }
     }
 }
