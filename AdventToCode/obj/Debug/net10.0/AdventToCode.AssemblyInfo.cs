9c376b3d08e2e2a4f59da26c5aee473e80ea8259
JonasBisig <jonas.bisig@gmx.ch>
2026-02-03 11:46:38 +0100

    Added Gitignore

--- AdventToCode/obj/Debug/net10.0/AdventToCode.AssemblyInfo.cs
+++ AdventToCode/obj/Debug/net10.0/AdventToCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventToCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd402135a265836fe73d13c04dced567aa006ffd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a693f85b99fe4a9e8ad8fa3965a71d88753ef7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventToCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventToCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
